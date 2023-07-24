ac1b90d8f0e72148c732d64c4123dfde3c76b754
unknown <bobbymcmaster2017@gmail.com>
2023-07-24 14:55:08 -0400

    added other visualization test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,13 @@ df = pd.DataFrame(
    columns=('col %d' % i for i in range(5)))
 
 st.table(df)
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
